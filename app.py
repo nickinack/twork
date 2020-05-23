aaf39981f87e0bc651a30516bc92847bbcd994f6
abhishekh <abhishekh2001@gmail.com>
2020-05-23 22:47:09 +0530

    Users Database

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask , render_template , request
+
 app = Flask(__name__)
+app.secret_key = 'test'  # app.secret_key = 'a26d9e7eff0d4dba88d35458034c57b1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, '../dbase.sqlite')
+
+
+# Import database and models
+from models import db, User
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
--- models.py
+++ models.py
@@ -1,7 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy, event
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstname = db.Column(db.String(24), nullable=False)
+    lastname = db.Column(db.String(24))
+    username = db.Column(db.String(24), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+
+
+@event.listens_for(User.password, 'set', retval=True)
+def hash_user_password(target, value, oldvalue, initiator):
+    if value != oldvalue:
+        return generate_password_hash(value)
+    return value
+
+
 class todo(db.Model):
     id = db.Column(db.Integer , primary_key=True)
     text = db.Column(db.String(200))
     desc = db.Column(db.String(400))
     deadline = db.Column(db.Date)
     complete = db.Column(db.Boolean)
-    
+
