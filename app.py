42081493a20579439efaa88e9ba8f7028b1a7087
abhishekh <abhishekh2001@gmail.com>
2020-05-23 23:07:50 +0530

    added authentication

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request, session, redirect, g, flash, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 app.secret_key = 'test'  # app.secret_key = 'a26d9e7eff0d4dba88d35458034c57b1'
@@ -11,6 +13,82 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+@app.route('/')
+def homepage():
+    return render_template("index.html");
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    flag = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        flag = None
+
+        if username is None:
+            flag = 'Username is required.'
+        elif password is None:
+            flag = 'Password is required.'
+        elif User.query.filter_by(username=username).first():
+            flag = 'Username must be unique'
+        elif User.query.filter_by(email=email).first():
+            flag = 'Email is already registered'
+        else:
+            user = User(
+                firstname=request.form['firstname'],
+                lastname=request.form['lastname'],
+                email=request.form['email'],
+                username=request.form['username'],
+                password=request.form['password']
+            )
+            db.session.add(user)
+            db.session.commit()
+            print('committed')
+            return redirect(url_for('login'))
+    
+    if flag:
+        flash(flag)
+        print(flag)
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    flag = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user is None:
+            flag = 'No such user'
+        elif not check_password_hash(user.password, password):
+            flag = 'Incorrect password'
+        
+        if flag:
+            flash(flag)
+        else:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('register'))
+    
+    return render_template('login.html')
+
+
+@app.before_request
+def things_to_do():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        user = User.query.get(user_id)
+        g.user = user
 
 
 @app.route('/')
