c7045044e35a058c5f3087097dc870544bc32564
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-05-24 03:09:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,29 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy, event
-from werkzeug.security import generate_password_hash, check_password_hash
-
-db = SQLAlchemy()
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    firstname = db.Column(db.String(24), nullable=False)
-    lastname = db.Column(db.String(24))
-    username = db.Column(db.String(24), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(), unique=True, nullable=False)
-
-
-@event.listens_for(User.password, 'set', retval=True)
-def hash_user_password(target, value, oldvalue, initiator):
-    if value != oldvalue:
-        return generate_password_hash(value)
-    return value
-
-
-class todo(db.Model):
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+#Instead of including foriegn key, we use login manager to find current user and then have current username dumped into database
+#Will do it tomorrow
+class Todo(db.Model):
     id = db.Column(db.Integer , primary_key=True)
-    text = db.Column(db.String(200))
+    text = db.Column(db.String(200) , nullable = 'False')
     desc = db.Column(db.String(400))
-    deadline = db.Column(db.Date)
+    deadline = db.Column(db.String(24) , nullable = 'False')
     complete = db.Column(db.Boolean)
 
+class User(db.Model):
+    __bind_key__ = 'user'
+    id = db.Column(db.Integer , primary_key=True)
+    email = db.Column(db.String() , nullable = False , unique=True)
+    username = db.Column(db.String(120) , nullable = False , unique=True)
+    password = db.Column(db.String(120) , nullable = False)
+    firstname = db.Column(db.String(120) , nullable = False)
+    lastname = db.Column(db.String(120) , nullable = False)
+
+    def is_active(self):
+       return True
