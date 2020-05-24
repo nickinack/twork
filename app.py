b25381d420f5b04aeace33a9e9da1f37f5773236
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-05-24 18:12:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from models import db, User
+from models import db, User, Todo
 db.init_app(app)
 
 with app.app_context():
